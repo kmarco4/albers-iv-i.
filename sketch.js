a7dcff20aea585a4c0191024c19df64e02e30d0d
kmarco4 <113069202+kmarco4@users.noreply.github.com>
2022-09-28 16:57:15 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+let clrs = [
+'#E27F34', //top 0
+'#FEF254', //middle top 1
+'#A36C33', //middle strip 2
+'#042060', //middle bottom 3
+'#3778AE' //bottom 4
+]
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+}
+
+function draw() {
+ background(220);
+ canvasRect(clrs[4], height/2, height/2, false);
+ canvasRect(clrs[0], height/2, 0, false);
+ fill(clrs[2]); 
+ rect(width * 1/3, height * 1/6, width * 1/3, height * 2/3); 
+ canvasRect(clrs[1], height/6, height/3, true);
+ canvasRect(clrs[3], height/6, height/2, true);
+
+
+}
+
+function canvasRect(clr, hgt, y_offset, clickable) {
+	fill(clr);
+	if((clickable == true) && (mouseIsPressed == false)){
+		rect(0, y_offset, width, hgt);
+	} 
+	if(clickable == false){
+		rect(0, y_offset, width, hgt);
+	}
+}
